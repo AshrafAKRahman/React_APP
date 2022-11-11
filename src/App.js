bff314443254dcf75290690d436b26b3dc72e333
Ash <ashraf.ak.rahman@gmail.com>
2022-11-11 07:52:10 +0000

    My First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Product from './Product';
+
+function App() {
+  return(
+    <div>
+      <h1>Hello World</h1>
+    <Product name='Amazon Echo' description='Your AI assistant' price={59.99}/>
+    <Product name='Amazon Echo' description='Your AI assistant' price={59.99}/>
+    <Product name='Amazon Echo' description='Your AI assistant' price={59.99}/>
+    <Product name='Amazon Echo' description='Your AI assistant' price={59.99}/>
+      {/*product name, description, price */}
+      {/*product name, description, price */}
+      {/*product name, description, price */}
+      {/*product name, description, price */}
+    </div>
+  )
+}
+
+export default App;
